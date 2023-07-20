9c08ba95edf931efb180bda5f8fb67f03d768f9e
brunoahcor2 <bruno.ahcor2@gmail.com>
2023-07-20 00:35:10 -0300

    Changes

--- back/src/test/resources/scripts/person_inserts.sql
+++ back/src/test/resources/scripts/person_inserts.sql
@@ -1,7 +1,7 @@
-INSERT INTO person (name, date_birth, cpf, employee) VALUES
-('Moheu','1985-12-09','05691930010',true),
-('Zutoatya','1984-11-01','82927089019',true),
-('Gacayoss','1983-10-05','72860478094',true),
-('Fuynu','1982-09-15','83203556057',true),
-('Nuzae','1981-08-02','73481093063',false),
-('Daykeopi','1983-10-05','37265854004',false);
+INSERT INTO person (name, date_birth, cpf, employee, position) VALUES
+('Moheu','1985-12-09','05691930010',true,'MANAGER'),
+('Zutoatya','1984-11-01','82927089019',true,'MANAGER'),
+('Gacayoss','1983-10-05','72860478094',true,'ANALYST'),
+('Fuynu','1982-09-15','83203556057',true,'ANALYST'),
+('Nuzae','1981-08-02','73481093063',false,''),
+('Daykeopi','1983-10-05','37265854004',false,'');
