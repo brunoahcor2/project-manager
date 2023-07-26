66d3df2f83133941f1a53593da07ff84c6548ab0
brunoahcor2 <bruno.ahcor2@gmail.com>
2023-07-25 23:19:57 -0300

    Added pipes

---
+++ front/src/app/shared/pipes/yes-or-no.pipe.ts
@@ -0,0 +1,6 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'yesOrNo' })
+export class YesOrNoPipe implements PipeTransform {
+  transform(value: boolean): string { return value ? 'Yes' : 'No'; }
+}
