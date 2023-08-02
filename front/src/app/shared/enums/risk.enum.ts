9d6b0a48566c5294c5beb9d5c6f7f5f7ace51c0c
brunoahcor2 <bruno.ahcor2@gmail.com>
2023-08-01 21:33:54 -0300

    Add enums

---
+++ front/src/app/shared/enums/risk.enum.ts
@@ -0,0 +1,5 @@
+export enum Risk {
+  LOW,
+  MEDIUM,
+  HIGH
+}
---
+++ front/src/app/shared/enums/status.enum.ts
@@ -0,0 +1,10 @@
+export enum Status {
+    EM_ANALISE,
+    ANALISE_REALIZADA,
+    ANALISE_APROVADA,
+    INICIADO,
+    PLANEJADO,
+    EM_ANDAMENTO,
+    ENCERRADO,
+    CANCELADO
+}
